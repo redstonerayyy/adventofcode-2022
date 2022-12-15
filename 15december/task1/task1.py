75e3b6f5cffd43ce6b82c8ef1e30c032aa706c1e
Redstonerayy <antonpaulr@protonmail.com>
2022-12-15 20:04:50 +0100

    idk do some other projects now

--- 15december/task1/task1.py
+++ 15december/task1/task1.py
@@ -1,6 +1,6 @@
 import os
 
-with open(os.path.join(os.path.dirname(__file__), "test.txt")) as file:
+with open(os.path.join(os.path.dirname(__file__), "input.txt")) as file:
 	lines = [line.replace("\n", "") for line in file if line.replace("\n", "") != ""]
 	sensorpairs = []
 	for sensor in lines:
@@ -23,28 +23,40 @@ xmax = max(xcords)
 yheight = 10
 ycordsfilled = []
 for pair in sensorpairs:
-	# if pair[0] == 8:
+	# if pair[0] == 20:
 	# 	breakpoint()
 	if pair[1] <= yheight:
 		shift = 0
-		width = pair[4] * 2
-		while pair[1] + shift <= pair[1] + pair[5]:
+		width = (pair[4] + pair[5]) * 2 + 1
+		while pair[1] + shift <= pair[1] + (pair[4] + pair[5]):
 			if pair[1] + shift == yheight:
-				ycordsfilled.append([width, pair[0], pair[1]])
+				ycordsfilled.append([width, pair[0]])
 				break		
 			width -= 2
 			shift += 1
 		
 	elif pair[1] > yheight:
 		shift = 0
-		width = 1
-		while pair[1] - shift >= pair[1] - pair[5]:
+		width = (pair[4] + pair[5]) * 2 + 1
+		while pair[1] - shift >= pair[1] - (pair[4] + pair[5]):
 			if pair[1] - shift == yheight:
-				ycordsfilled.append([width, pair[0], pair[1]])
-				break		
-			width += 2
+				ycordsfilled.append([width, pair[0]])
+				break
+			width -= 2
 			shift += 1
-# calculate width
 
-print(ycordsfilled)
+# calculate width
+allcords = []
+for cords in ycordsfilled:
+    w = (cords[0] - 1)//2
+    for i in range(-w, w):
+        allcords.append(i + cords[1])
+
+allset = set(allcords)
+row2mil = [i for i in range(xmin, xmax + 1)]
+row2milset = set(row2mil)
+intersection = allset & row2milset
+intersectlist = list(intersection)
+print(len(intersectlist))
+
 
